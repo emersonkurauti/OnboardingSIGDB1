b86686e7d135d40215645ae2433ea6dc1ef93276
e <emerson.m.k@hotmail.com>
2020-10-26 15:38:46 -0300

    DataContext

--- OnboardingSIGDB1.API/Startup.cs
+++ OnboardingSIGDB1.API/Startup.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
+using OnboardingSIGDB1.Data;
 
 namespace OnboardingSIGDB1.API
 {
@@ -19,7 +21,8 @@ namespace OnboardingSIGDB1.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddDbContext<DataContext>(x => x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             services.AddSwaggerGen(c => {
                 c.SwaggerDoc("v1",
---
+++ OnboardingSIGDB1.Data/DataContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OnboardingSIGDB1.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+    }
+}
