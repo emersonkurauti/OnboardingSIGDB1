c31f276b7e82ea65540c1d9016dc60413b1537f0
e <emerson.m.k@hotmail.com>
2020-10-29 10:49:23 -0300

    Criar restrição na exclusão do cargo

---
+++ OnboardingSIGDB1.Data/Migrations/20201029134834_OnDelete retrict na tabela relacionamento.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnboardingSIGDB1.Data.Migrations
+{
+    public partial class OnDeleteretrictnatabelarelacionamento : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_FuncionariosCargos_Cargos_CargoId",
+                table: "FuncionariosCargos");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FuncionariosCargos_Cargos_CargoId",
+                table: "FuncionariosCargos",
+                column: "CargoId",
+                principalTable: "Cargos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_FuncionariosCargos_Cargos_CargoId",
+                table: "FuncionariosCargos");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FuncionariosCargos_Cargos_CargoId",
+                table: "FuncionariosCargos",
+                column: "CargoId",
+                principalTable: "Cargos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
