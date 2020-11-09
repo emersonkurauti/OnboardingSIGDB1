5eac379fcf27b9c7e80338dc3c3bdbf633cb1c59
e <emerson.m.k@hotmail.com>
2020-11-09 08:16:24 -0300

    Primeiros testes de unidade

---
+++ OnboardingSIGDB1.DomainTest/TestarCargos.cs
@@ -0,0 +1,22 @@
+﻿using OnboardingSIGDB1.Domain.Entitys;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace OnboardingSIGDB1.DomainTest
+{
+    public class TestarCargos
+    {
+        [Theory(DisplayName = "TestarCriacaoCurso")]
+        [InlineData("")]
+        [InlineData(null)]
+        [InlineData("Descrição de cargo com mais de 250 caracteres ................................................................................................................................................................................................................")]
+        public void TestarCriacaoCursoDescricaoInvalida(string descricao)
+        {
+            Cargo cargo = new Cargo();
+            cargo.Descricao = descricao;
+            Assert.False(cargo.Validar());
+        }
+    }
+}
---
+++ OnboardingSIGDB1.DomainTest/TestarValidadoresDocumentos.cs
@@ -0,0 +1,46 @@
+﻿using OnboardingSIGDB1.Domain.Utils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace OnboardingSIGDB1.DomainTest
+{
+    public class TestarValidadoresDocumentos
+    {
+
+        [Fact(DisplayName = "TestarValidadorCPFValido")]
+        public void TestarValidadorCPFValido()
+        {
+            var cpf = "33214080822";
+
+            Assert.True(ValidadorCPF.ValidaCPF(cpf));
+        }
+
+
+        [Fact(DisplayName = "TestarValidadorCPFInvalido")]
+        public void TestarValidadorCPFInvalido()
+        {
+            var cpf = "33210080822";
+
+            Assert.False(ValidadorCPF.ValidaCPF(cpf));
+        }
+
+        [Fact(DisplayName = "TestarValidadorCNPJValido")]
+        public void TestarValidadorCNPJValido()
+        {
+            var cnpj = "92349340000140";
+
+            Assert.True(ValidadorCPNJ.ValidaCNPJ(cnpj));
+        }
+
+
+        [Fact(DisplayName = "TestarValidadorCNPJInvalido")]
+        public void TestarValidadorCNPJInvalido()
+        {
+            var cnpj = "92349340000100";
+
+            Assert.False(ValidadorCPNJ.ValidaCNPJ(cnpj));
+        }
+    }
+}
