9b78077e8d0c2bbbab88fcb5302a45838a34d1e4
e <emerson.m.k@hotmail.com>
2020-11-13 15:23:28 -0300

    Teste para o método Alterar

--- OnboardingSIGDB1.DomainTest/Services/TestarCargoService.cs
+++ OnboardingSIGDB1.DomainTest/Services/TestarCargoService.cs
@@ -5,6 +5,7 @@ using OnboardingSIGDB1.Domain.Dto;
 using OnboardingSIGDB1.Domain.Entitys;
 using OnboardingSIGDB1.Domain.Notifications;
 using OnboardingSIGDB1.Domain.Services.Cargos;
+using OnboardingSIGDB1.DomainTest.Builders;
 using System;
 using System.Linq.Expressions;
 using Xunit;
@@ -18,13 +19,14 @@ namespace OnboardingSIGDB1.DomainTest.Services
         private Mock<IRepository<Cargo>> _cargoRepositoryMock;
         private NotificationContext _notification;
         private GravarCargoService _gravarCargoService;
+        private Faker _fake;
 
         public TestarCargoService()
         {
-            var fake = new Faker();
+            _fake = new Faker();
             _cargoDTO = new CargoDTO
             {
-                Descricao = fake.Random.Words(2)
+                Descricao = _fake.Random.Words(2)
             };
 
             _cargoRepositoryMock = new Mock<IRepository<Cargo>>();
@@ -50,6 +52,38 @@ namespace OnboardingSIGDB1.DomainTest.Services
             _gravarCargoService.Adicionar(_cargoDTO);
 
             _cargoRepositoryMock.Verify(m => m.Add(
+                It.Is<Cargo>(c => c.Descricao == _cargoDTO.Descricao)
+            ), Times.Never());
+        }
+
+        [Fact]
+        public void TestarDeveAlterarCargo()
+        {
+            var cargo = CargoBuilder.Novo().Build();
+            _cargoRepositoryMock.Setup(r => r.Get(It.IsAny<Expression<Func<Cargo, bool>>>())).Returns(cargo);
+
+            while (cargo.Descricao.Equals(_cargoDTO.Descricao))
+                _cargoDTO.Descricao = _fake.Random.Words(2);
+
+            _gravarCargoService.Alterar(0, _cargoDTO);
+
+            _cargoRepositoryMock.Verify(m => m.Update(
+                It.Is<Cargo>(c => c.Descricao == _cargoDTO.Descricao)
+            ));
+        }
+
+        [Fact]
+        public void TestarNaoDeveAlterarCargoMesmaDescricaoDeOutroJaSalvo()
+        {
+            var cargo = CargoBuilder.Novo().Build();
+            _cargoRepositoryMock.Setup(r => r.Get(It.IsAny<Expression<Func<Cargo, bool>>>())).Returns(cargo);
+
+            _cargoDTO.Descricao = cargo.Descricao;
+            _cargoRepositoryMock.Setup(r => r.Exist(It.IsAny<Expression<Func<Cargo, bool>>>())).Returns(true);
+
+            _gravarCargoService.Alterar(0, _cargoDTO);
+
+            _cargoRepositoryMock.Verify(m => m.Update(
                 It.Is<Cargo>(c => c.Descricao == _cargoDTO.Descricao)
             ), Times.Never());
         }
